3fc0713251e16039680aed276ea0dcc9899a4def
shibing624 <shibing624@126.com>
2022-06-08 19:00:03 +0800

    update extraction.

--- relext/information_extraction.py
+++ relext/information_extraction.py
@@ -8,7 +8,7 @@ from relext.uie_predictor import UIEPredictor
 
 
 class InformationExtraction:
-    def __init__(self, static_model_dir, schema=None, max_seq_len=512, position_prob=0.5, device='cpu'):
+    def __init__(self, static_model_dir, schema=(), max_seq_len=512, position_prob=0.5, device='cpu'):
         self.predictor = UIEPredictor(static_model_dir, schema, max_seq_len, position_prob, device)
 
     def extract(self, texts, schema=None):
@@ -21,6 +21,8 @@ class InformationExtraction:
         if schema:
             # Reset schema
             self.predictor.set_schema(schema)
+        if self.predictor._schema_tree is None:
+            raise RuntimeError('Schema is not set')
         outputs = self.predictor.predict(texts)
 
         return outputs
